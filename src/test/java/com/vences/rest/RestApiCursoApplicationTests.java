b5333553597eb12b9df915149b6eee72d977d899
David Vences <dvences90@gmail.com>
2020-11-03 09:59:28 -0600

    Primer Commit

---
+++ src/test/java/com/vences/rest/RestApiCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vences.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
